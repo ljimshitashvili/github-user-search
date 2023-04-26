ba8670aa0320e61c1aa2637a692cd563a2584fe8
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-04-26 04:42:39 +0400

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App px-[24px] min-h-screen bg-[#F2F2F2]">
+      <Header />
       <Search />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/Search.tsx", "./src/App.tsx"],
+  content: [
+    "./src/components/Search.tsx",
+    "./src/App.tsx",
+    "./src/components/Header.tsx",
+  ],
   theme: {
     extend: {},
   },
